b64eca8d632140da660df64abb638bb8402712f8
zeezat <azeezatmaja@gmail.com>
2023-04-03 17:04:50 +0000

    task complete

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''async routine called wait_n that takes in 2 int arguments.
+'''
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''wait_n should return the list of all the delays
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
